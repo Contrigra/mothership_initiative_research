2c7b9cc45dbd9f1e61f3b37734f626465f848085
Contrigra <gaponov60b@gmail.com>
2021-08-26 00:39:34 +0300

    fix: fixed a bug, as having equal roll to speed should've lead to a win as well.

--- mothership_initiative_research.py
+++ mothership_initiative_research.py
@@ -9,11 +9,10 @@ def just_round_wins(speed: int, n_combats: int = 100, combat_rounds: int = 3):
     # changing to two  for proper range further
     if combat_rounds == 1:
         combat_rounds = 2
-
     round_wins = 0
     for combat in range(n_combats - 1):
         for _ in range(combat_rounds - 1):
-            if speed > random.randint(0, 99):
+            if speed >= random.randint(0, 99):
                 round_wins += 1
 
     return round_wins
@@ -25,7 +24,7 @@ def full_combat_wins(speed: int, n_combats: int = 100,
     :returns int: number of winning rounds'''
     full_combat_wins = 0
     for combat in range(n_combats - 1):
-        if speed > random.randint(0, 99):
+        if speed >= random.randint(0, 99):
             full_combat_wins += 1
 
     return full_combat_wins * combat_rounds
@@ -35,7 +34,7 @@ speed = 36
 combat_rounds = 3
 just_rounds = []
 full_combats = []
-number_of_combats = 1000
+number_of_combats = 100
 
 # creating lists with data.
 for _ in range(number_of_combats - 1):
@@ -49,3 +48,4 @@ print(f'Rounds:     {combat_rounds}')
 print(f'PC'f' speed:   {speed}')
 print(f'Each round: {mean(just_rounds)}')
 print(f'Just once:  {mean(full_combats)}')
+
